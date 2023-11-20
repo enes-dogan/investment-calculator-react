7d8cec60183707034e7c781273512fa92acd7267
enes-dogan <enesdogan210@gmail.com>
2023-11-20 23:52:21 +0300

    project reset

--- src/types.ts
+++ src/types.ts
@@ -1,28 +1,6 @@
-export interface UserInput {
-  currentSavings: string;
-  yearlySavings: string;
-  expectedInterest: string;
-  investmentDuration: string;
-}
-
-export interface InvestmentFormProps {
-  userInputs: UserInput;
-  onInputChange: (idf: string, value: string) => void;
-  onFormReset: () => void;
-  onCalculate: () => void;
-}
-
-export interface InvestmentData {
-  year: number;
-  yearlyInterest: string;
-  savingsEndOfYear: string;
-  yearlyContribution: string;
-}
-
-export interface ResultsTableProps {
-  investmentsByYear: InvestmentData[];
-}
-
-export interface YearlyResultProps {
-  investment: InvestmentData;
+export interface calculateInvestmentResultsProps {
+  initialInvestment: number;
+  annualInvestment: number;
+  expectedReturn: number;
+  duration: number;
 }
